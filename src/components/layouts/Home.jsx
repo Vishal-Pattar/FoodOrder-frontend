e1b5e04e5edcf1ed2d097296ec3361298869dc18
Vishal-Pattar <vmpnwc15002@gmail.com>
2024-07-26 21:34:07 +0530

    Updated Home.jsx

--- src/components/layouts/Home.jsx
+++ src/components/layouts/Home.jsx
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import CountRestaurant from './CountRestaurant';
 import Restaurant from './Restaurant';
+import { getRestaurants } from '../../actions/restaurantAction';
+import { useDispatch } from 'react-redux';
 
 export default function Home() {
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getRestaurants());
+  }, []);
+  
   return (
     <>
       <CountRestaurant />
