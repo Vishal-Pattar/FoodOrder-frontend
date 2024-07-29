7886624ce4c423883fb46d36c908bf660887c2b8
Vishal-Pattar <vmpnwc15002@gmail.com>
2024-07-29 23:14:47 +0530

    Updated restaurantAction.js

--- src/actions/restaurantAction.js
+++ src/actions/restaurantAction.js
@@ -7,7 +7,7 @@ export const getRestaurants = () => {
             dispatch({ type: ALL_RESTAURANTS_REQUEST });
             let link = `api/v1/eats/stores`;
             const { data } = await axios.get(link);
-            console.log(data);
+            // console.log(data);
             const { restaurants, count } = data;
             dispatch({
                 type: ALL_RESTAURANTS_SUCCESS,
