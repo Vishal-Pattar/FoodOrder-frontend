d5d0a5a9bba6b17374481c9eef3708f7642eb530
Vishal-Pattar <vmpnwc15002@gmail.com>
2024-07-29 23:16:53 +0530

    Updated CountRestaurant.jsx

--- src/components/layouts/CountRestaurant.jsx
+++ src/components/layouts/CountRestaurant.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { getRestaurants } from '../../actions/restaurantAction';
+import Loader from './Loader';
+import Message from './Message';
 
 export default function CountRestaurant() {
-  return (
+  const { loading: countRestaurantsLoading, error: countRestaurantsError, count: countRestaurants } = useSelector(state => state.restaurants);
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getRestaurants());
+  }, [dispatch]);
+
+  return (countRestaurantsLoading ? (
+    <Loader />
+  ) : countRestaurantsError ? (
+    <Message variant="danger">{countRestaurantsError}</Message>
+  ) : (
     <div>
-        <p className='NumOfRestro'>
-            8 <span className='Restro'>Restaurant</span>
-        </p>
-        <hr />
+      <p className='NumOfRestro'>
+        {countRestaurants} <span className='Restro'>Restaurant</span>
+      </p>
+      <hr />
     </div>
+  )
   )
 }
