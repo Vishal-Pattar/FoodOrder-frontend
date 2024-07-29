f98ca2ea5d4a6ae7010e47d22e947053369980dc
Vishal-Pattar <vmpnwc15002@gmail.com>
2024-07-29 23:15:23 +0530

    Updated Restaurant.jsx

--- src/components/layouts/Restaurant.jsx
+++ src/components/layouts/Restaurant.jsx
@@ -1,23 +1,21 @@
 import React from 'react'
 
-export default function Restaurant() {
-  return (
-    <div className='col-sm-12 col-md-6 col-lg-3 my-3'>
-        <div className="card p-3 rounded">
-            <img 
-            src="https://b.zmtcdn.com/data/pictures/chains/6/10506/5ba610384e59e9f69e718b106b2d28c2.jpg?output-format=webp&fit=around|300:273&crop=300:273;*,*" 
-            alt="Dominoz" />
-            <div className="card-body d-flex flex-column">
-                <h5 className="card-title">Dominos Pizza</h5>  
-                <p className="rest_address">
-                    123 Street, Place, City - 000000, State
-                </p>  
-                <div className="rating-outer">
-                    <div className="rating-inner"></div>
-                    <span id="no_of_reviews">(140 reviews)</span>    
-                </div>            
+export default function Restaurant({ restaurant }) {
+    return (
+        <div className='col-sm-12 col-md-6 col-lg-3 my-3'>
+            <div className="card p-3 rounded">
+                <img src={restaurant.images[0].url} alt={restaurant.name} className='card-img-top mx-auto' />
+                <div className="card-body d-flex flex-column">
+                    <h5 className="card-title">{restaurant.name}</h5>
+                    <p className="rest_address">{restaurant.address}</p>
+                    <div className='ratings mt-auto'>
+                        <div className="rating-outer">
+                            <div className="rating-inner" style={{ width: `${(restaurant.ratings / 5.0) * 100}%` }}></div>
+                        </div>
+                        <span id="no_of_reviews">({restaurant.numOfReviews} reviews)</span>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
