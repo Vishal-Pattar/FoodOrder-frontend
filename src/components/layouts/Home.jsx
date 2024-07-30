ccace529708fa748f8cb332baacda2295dfafe5b
Vishal-Pattar <vmpnwc15002@gmail.com>
2024-07-30 21:05:23 +0530

    Updated Home.jsx

--- src/components/layouts/Home.jsx
+++ src/components/layouts/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import CountRestaurant from './CountRestaurant';
 import Restaurant from './Restaurant';
-import { getRestaurants } from '../../actions/restaurantAction';
+import { getRestaurants, sortByReviews, sortByRatings, toggleVegOnly } from '../../actions/restaurantAction';
 import { useDispatch, useSelector } from 'react-redux';
 import Loader from './Loader';
 import Message from './Message';
@@ -9,11 +9,26 @@ import Message from './Message';
 export default function Home() {
 
   const dispatch = useDispatch();
-  const { loading: restaurantsLoading, error: restaurantsError, restaurants } = useSelector(state => state.restaurants);
+  const { loading: restaurantsLoading,
+    error: restaurantsError,
+    restaurants,
+    showVegOnly, } = useSelector(state => state.restaurants);
 
   useEffect(() => {
     dispatch(getRestaurants());
   }, [dispatch]);
+
+  const handleSortByReview = () => {
+    dispatch(sortByReviews());
+  }
+
+  const handleSortByRating = () => {
+    dispatch(sortByRatings());
+  }
+
+  const handleToggleVegOnly = () => {
+    dispatch(toggleVegOnly());
+  }
 
   return (
     <>
@@ -26,16 +41,23 @@ export default function Home() {
         <>
           <section>
             <div className="sort">
-              <button className="sort_veg p-3">Pure Veg</button>
-              <button className="sort_rev p-3">Sort by Review</button>
-              <button className="sort_rate p-3">Sort by Ratings</button>
+              <button className="sort_veg p-3" onClick={handleToggleVegOnly}>
+                {showVegOnly ? "Show All" : "Pure Veg"}
+              </button>
+              <button className="sort_rev p-3" onClick={handleSortByReview}>Sort by Review</button>
+              <button className="sort_rate p-3" onClick={handleSortByRating}>Sort by Ratings</button>
             </div>
           </section>
           <div className="row mt-4">
-            {restaurants ?
-              restaurants.map((restaurant) => (
-                <Restaurant key={restaurant._id} restaurant={restaurant} />
-              )) : null}
+            {restaurants ? (
+              restaurants.map((restaurant) =>
+                !showVegOnly || (showVegOnly && restaurant.isVeg) ? (
+                  <Restaurant key={restaurant._id} restaurant={restaurant} />
+                ) : null
+              )
+            ) : (
+              <Message variant="info">No Restaurant Found </Message>
+            )}
           </div>
         </>
       )}
