4dcb841e34d736a8ab90bcf7f93233797094b2d5
Vishal-Pattar <vmpnwc15002@gmail.com>
2024-08-01 19:38:01 +0530

    Updated FoodItem.jsx

--- src/components/layouts/FoodItem.jsx
+++ src/components/layouts/FoodItem.jsx
@@ -1,23 +1,22 @@
 import React from 'react'
 import { LiaRupeeSignSolid } from "react-icons/lia";
 
-export default function FoodItem() {
+export default function FoodItem({fooditem, restaurant}) {
+    console.log(fooditem);
     return (
         <div className='col-sm-12 col-md-6 col-lg-3 my-3'>
             <div className="card p-3 rounded">
                 <img
-                    src='https://b.zmtcdn.com/data/pictures/chains/6/10506/1adb116d088669540c89150836d668f9.jpg?output-format=webp&fit=around|300:273&crop=300:273;*,*'
-                    alt='Pizza'
+                    src={fooditem.images[0].url}
+                    alt={fooditem.name}
                     className="card-img-top mx-auto"
                 />
                 <div className="card-body d-flex flex-column">
-                    <h5 className="card-title">Veg Loaded Pizza</h5>
-                    <p className="fooditem_des">
-                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem numquam libero itaque ab, repellendus tenetur velit quis amet doloribus sit.
-                    </p>
+                    <h5 className="card-title">{fooditem.name}</h5>
+                    <p className="fooditem_des">{fooditem.description}</p>
                     <p className="card-text">
                         <LiaRupeeSignSolid />
-                        180
+                        {fooditem.price}
                         <br />
                     </p>
                     <button type='button' id='cart_btn' className='btn btn-primary d-inline ml-4'>
@@ -25,8 +24,8 @@ export default function FoodItem() {
                     </button>
                     <br />
                     <p>Status: {" "}
-                        <span id='stock_status' className={10 > 5 ? 'greenColor' : 'redColor'} >
-                            {10 > 5 ? 'In Stock' : 'Out of Stock'}
+                        <span id='stock_status' className={fooditem.stock ? 'greenColor' : 'redColor'} >
+                            {fooditem.stock ? 'In Stock' : 'Out of Stock'}
                         </span>
                     </p>
                 </div>
