a64c3111219cc0b9041623a480fcb81a0349c440
Vishal-Pattar <vmpnwc15002@gmail.com>
2024-08-01 19:54:56 +0530

    Updated Menu.jsx

--- src/components/layouts/Menu.jsx
+++ src/components/layouts/Menu.jsx
@@ -1,27 +1,49 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import FoodItem from './FoodItem'
+import { useDispatch, useSelector } from 'react-redux'
+import { useParams } from 'react-router-dom'
+import { getMenus } from '../../actions/menuAction'
+import Loader from './Loader'
+import Message from './Message'
 
 export default function Menu() {
-  return (
-    <div>
+    const { id } = useParams();
+    const dispatch = useDispatch();
+
+    const { loading, error, menus } = useSelector(state => state.menus);
+
+    useEffect(() => {
+        dispatch(getMenus(id))
+    }, [dispatch, id]);
+
+    console.log(menus);
+
+    return (
         <div>
-            <h2>Chaats</h2>
-            <hr />
-            <div className="row">
-                <FoodItem />
-                <FoodItem />
-                <FoodItem />
-            </div>
+            {loading ? <Loader /> : error ? (<Message varaint="danger">{error}</Message>) :
+                menus && menus.length > 0 ? (menus.map((menu) => (
+                    <div key={menu._id}>
+                        <h2>{menu.category}</h2>
+                        <hr />
+                        {menu.items && menu.items.length > 0 ? (
+                            <div className='row'>
+                                {menu.items.map((fooditem) => (
+                                    <FoodItem
+                                        key={fooditem._id}
+                                        fooditem={fooditem}
+                                        restaurant={id}
+                                    />
+                                ))}
+                            </div>
+                        ) : (
+                            <Message variant="info">No FoodItem found</Message>
+                        )}
+                    </div>
+                ))
+                ) : (
+                    <Message variant="info">No Menus Found</Message>
+                )
+            }
         </div>
-        <div>
-            <h2>Main Course</h2>
-            <hr />
-            <div className="row">
-                <FoodItem />
-                <FoodItem />
-                <FoodItem />
-            </div>
-        </div>
-    </div>
-  )
+    )
 }
