8e2c3122bf76669540d7949d174476c9cd4926a3
Vishal-Pattar <vmpnwc15002@gmail.com>
2024-08-05 23:10:30 +0530

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/layouts/Header";
@@ -8,22 +8,35 @@ import Menu from "./components/layouts/Menu";
 import Cart from "./components/cart/Cart";
 import Login from "./components/users/Login";
 import Register from "./components/users/Register";
+import ForgotPassword from "./components/users/ForgotPassword";
+import store from "./store";
+import { loadUser } from "./actions/userAction";
+import Profile from "./components/users/Profile";
+import UpdateProfile from "./components/users/UpdateProfile";
 
 export default function App() {
-  return (
-  <Router>
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
 
-    <div className="App">
-      <Header />
-      <div className="container container-fluid">
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/eat/stores/:id/menus' element={<Menu />}/>
-          <Route path='/users/login' element={<Login />} />
-          <Route path='/users/signup' element={<Register />} />
-        </Routes>
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <div className="container container-fluid">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/eat/stores/:id/menus" element={<Menu />} />
+            <Route path="/users/login" element={<Login />} />
+            <Route path="/users/signup" element={<Register />} />
+            <Route path="/users/me" element={<Profile />} />
+            <Route path="/users/me/update" element={<UpdateProfile />} />
+            <Route path="/users/forgotPassword" element={<ForgotPassword />} />
+          </Routes>
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>;
-  </Router>
-)}
+      ;
+    </Router>
+  );
+}
