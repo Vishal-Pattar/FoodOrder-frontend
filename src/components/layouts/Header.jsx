2767368b9bb128f2f1e2be21da15eb5284e1fb1a
Vishal-Pattar <vmpnwc15002@gmail.com>
2024-08-07 05:01:21 +0530

    Updated Header.jsx

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import Search from './Search'
-import { Link } from 'react-router-dom'
-import { useAlert } from 'react-alert'
-import { useDispatch, useSelector } from 'react-redux'
-import { logout } from '../../actions/userAction'
+import React from "react";
+import Search from "./Search";
+import { Link } from "react-router-dom";
+import { useAlert } from "react-alert";
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../../actions/userAction";
 
 export default function Header() {
   const alert = useAlert();
@@ -13,8 +13,8 @@ export default function Header() {
 
   const logoutHandler = () => {
     dispatch(logout());
-    alert.success('Logged out successfully');
-  }
+    alert.success("Logged out successfully");
+  };
 
   return (
     <nav className="navbar row sticky-top">
@@ -36,34 +36,48 @@ export default function Header() {
         {user ? (
           <>
             <div className="ml-4 dropdown d-inline">
-              <Link to='/'
-                className='btn dropdown-toggle text-white mr-4'
-                type='button'
-                id='dropDownMenuButton'
-                data-toggle='dropdown'
-                aria-haspopup='true'
-                aria-expanded='false'>
-
+              <Link
+                to="/"
+                className="btn dropdown-toggle text-white mr-4"
+                type="button"
+                id="dropDownMenuButton"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
                 <figure className="avatar avatar-nav">
-                  <img src="/images/images.png" alt="" className='rounded-circle' />
+                  <img
+                    src={user.avatar.url}
+                    alt=""
+                    className="rounded-circle"
+                  />
                 </figure>
                 <span>{user && user.name}</span>
               </Link>
-              <div className='dropdown-menu' aria-labelledby='dropDownMenuButton'>
-                <Link to='eats/orders/me/myOrders' className='dropdown-item'>Orders</Link>
-                <Link to='users/me' className='dropdown-item'>Profile</Link>
-                <Link to='/' className='dropdown-item' onClick={logoutHandler}>Logout</Link>
+              <div
+                className="dropdown-menu"
+                aria-labelledby="dropDownMenuButton"
+              >
+                <Link to="eats/orders/me/myOrders" className="dropdown-item">
+                  Orders
+                </Link>
+                <Link to="users/me" className="dropdown-item">
+                  Profile
+                </Link>
+                <Link to="/" className="dropdown-item" onClick={logoutHandler}>
+                  Logout
+                </Link>
               </div>
             </div>
           </>
         ) : (
-          !loading &&
-          <Link to='/users/login' className="btn ml-4" id="login_btn">
-            Login
-          </Link>
-        )
-        }
+          !loading && (
+            <Link to="/users/login" className="btn ml-4" id="login_btn">
+              Login
+            </Link>
+          )
+        )}
       </div>
     </nav>
-  )
+  );
 }
