2f9f5240113e91fee68898b7a67be0aeb28e890c
Vishal-Pattar <vmpnwc15002@gmail.com>
2024-08-07 05:01:22 +0530

    Updated store.js

--- src/store.js
+++ src/store.js
@@ -1,23 +1,32 @@
 import {
-    legacy_createStore as createStore,
-    combineReducers,
-    applyMiddleware,
-    compose,
-} from 'redux';
-import thunk from 'redux-thunk';
-import { restaurantReducer } from './reducer/restaurantReducer';
-import { menuReducer } from './reducer/menuReducer';
-import { authReducer } from './reducer/userReducer';
+  legacy_createStore as createStore,
+  combineReducers,
+  applyMiddleware,
+  compose,
+} from "redux";
+import thunk from "redux-thunk";
+import { restaurantReducer } from "./reducer/restaurantReducer";
+import { menuReducer } from "./reducer/menuReducer";
+import {
+  authReducer,
+  forgotPasswordReducer,
+  userReducer,
+} from "./reducer/userReducer";
 
 const reducer = combineReducers({
-    restaurants: restaurantReducer,
-    menus: menuReducer,
-    auth: authReducer,
+  restaurants: restaurantReducer,
+  menus: menuReducer,
+  auth: authReducer,
+  user: userReducer,
+  forgotPassword: forgotPasswordReducer,
 });
 
 const composeenhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const middleware = [thunk];
-const store = createStore(reducer, composeenhancer(applyMiddleware(...middleware)));
+const store = createStore(
+  reducer,
+  composeenhancer(applyMiddleware(...middleware))
+);
 
 export default store;
